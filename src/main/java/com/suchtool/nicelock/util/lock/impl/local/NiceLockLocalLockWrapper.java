d8aeebdea244f6eac573b9f056538c7bed9cfae7
knifeout <2716552863@qq.com>
2024-12-05 17:27:23 +0800

    1.1.4版本发布（修复本地锁问题）

--- src/main/java/com/suchtool/nicelock/util/lock/impl/local/NiceLockLocalLockWrapper.java
+++ src/main/java/com/suchtool/nicelock/util/lock/impl/local/NiceLockLocalLockWrapper.java
@@ -2,12 +2,11 @@ package com.suchtool.nicelock.util.lock.impl.local;
 
 import lombok.Data;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 
 @Data
 public class NiceLockLocalLockWrapper {
-    private AtomicInteger count = new AtomicInteger(0);
+    private int count = 0;
 
     private Lock lock;
 }
